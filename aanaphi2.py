ae36b79a78161baf0db6116f8ead1351da3a5507
Chris Mann <chrispmann@gmail.com>
2024-02-15 14:13:12 -0800

    added annaphy2.py

---
+++ aanaphi2.py
@@ -0,0 +1,35 @@
+#Load model
+import transformers, torch
+
+torch.cuda.empty_cache()
+
+compute_dtype = torch.float16
+cache_path    = ''
+device        = 'cuda'
+model_id      = "mobiuslabsgmbh/aanaphi2-v0.1"
+model         = transformers.AutoModelForCausalLM.from_pretrained(model_id, torch_dtype=compute_dtype, 
+                                                                  cache_dir=cache_path,
+                                                                  device_map=device)
+tokenizer     = transformers.AutoTokenizer.from_pretrained(model_id, cache_dir=cache_path)
+
+
+model = model.half()
+
+#Set Prompt format
+instruction_template = "### Human: "
+response_template    = "### Assistant: "
+def prompt_format(prompt):
+    out = instruction_template + prompt + '\n' + response_template
+    return out
+model.eval();
+
+@torch.no_grad()
+def generate(prompt, max_length=500):
+    prompt_chat = prompt_format(prompt)
+    inputs      = tokenizer(prompt_chat, return_tensors="pt", return_attention_mask=True).to('cuda')
+    outputs     = model.generate(**inputs, max_length=max_length, eos_token_id= tokenizer.eos_token_id) 
+    text        = tokenizer.batch_decode(outputs[:,:-1])[0]
+    return text
+
+#Generate
+print(generate(input("prompt: ")))
